d33314343fe02648cd6dffe4291c9ee3b89cfca2
Daria Borodina <d.borodina@g.nsu.ru>
2021-03-04 13:55:00 +0700

    Оптимизировала, убрав промежуточный список lines

--- task3_1.py
+++ task3_1.py
@@ -1,16 +1,14 @@
 import json
 
 with open('wikidata_1000.json', 'r') as f:
-    lines = []
+    my_dict = {}
     for line in f:
-        lines.append(json.loads(line))
-my_dict = {}
-for l in lines:
-    word = l['label']['value']
-    if 'description' in l.keys():
-        desc = l['description']['value']
-    else:
-        desc = None
-    my_dict[word] = desc
+        l = json.loads(line)
+        word = l['label']['value']
+        if 'description' in l.keys():
+            desc = l['description']['value']
+        else:
+            desc = None
+        my_dict[word] = desc
 with open('new_dict.json', 'w') as f_write:
     json.dump(my_dict, f_write)
