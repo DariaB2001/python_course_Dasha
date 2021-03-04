1a38fe3f3f08c9dae7b4670de1eb7076f0fd7806
Daria Borodina <d.borodina@g.nsu.ru>
2021-03-04 14:24:07 +0700

    Избавилась от промежуточных списков

--- task4_1.py
+++ task4_1.py
@@ -1,10 +1,7 @@
 import xml.etree.ElementTree as etree
 tree = etree.parse('annot.opcorpora.no_ambig.xml')
 root = tree.getroot()
-phrases = []
-for source in root.iter('source'):
-    p = source.text
-    phrases.append(p)
 with open('phrases.txt', 'w') as f:
-    for phrase in phrases:
-        f.write(phrase + '\n')
+    for source in root.iter('source'):
+        p = source.text
+        f.write(p + '\n')
--- task4_2.py
+++ task4_2.py
@@ -2,12 +2,10 @@ import xml.etree.ElementTree as etree
 tree = etree.parse('annot.opcorpora.no_ambig.xml')
 root = tree.getroot()
 nouns_plural = []
-for token in root.iter('token'):
-    grammar = []  # грамматическая информация для каждого элемента token
-    for g in token.iter('g'):
-       grammar.append(g.get('v'))
-    if 'NOUN' in grammar and 'plur' in grammar:
-        nouns_plural.append(token.get('text'))
 with open('nouns_plural.txt', 'w') as f:
-    for noun in nouns_plural:
-        f.write(noun + ', ')
+    for token in root.iter('token'):
+        grammar = []  # грамматическая информация для каждого элемента token
+        for g in token.iter('g'):
+            grammar.append(g.get('v'))
+        if 'NOUN' in grammar and 'plur' in grammar:
+            f.write(token.get('text') + ', ')
