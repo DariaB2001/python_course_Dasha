32b349cefab60027cc1177faff33af8c99afbeb8
Daria Borodina <d.borodina@g.nsu.ru>
2021-04-10 22:36:05 +0700

    Избавилась от промежуточного цикла

--- task6_3.py
+++ task6_3.py
@@ -2,6 +2,7 @@ import collections
 import json
 with open('RomeoAndJuliet.json', 'r') as f:
     piece = json.load(f)
+cnt = collections.Counter()
 characters = []  # список, к которому буду добавлять имя персонажа столько раз, сколько раз он говорит что-нибудь
 acts = piece['acts']  # список всех актов пьесы
 for act in acts:  # в цикле перебираем акты пьесы (это словари)
@@ -10,10 +11,7 @@ for act in acts:  # в цикле перебираем акты пьесы (эт
         action = scene['action']  # это список из словарей; в каждом словаре по 2 ключа:
         # "character" (то есть имя персонажа) и "says" (то есть его реплика)
         for a in action:
-            characters.append(a['character'])
-cnt = collections.Counter()
-for c in characters:
-    cnt[c] += 1
+            cnt[a['character']] += 1
 list_d = list(cnt.items())
 list_d.sort(key=lambda t: t[1], reverse=True)
 for t in list_d:
