a241633109671d9e37e8ed22bf7aafbc4ab2f97d
Daria Borodina <d.borodina@g.nsu.ru>
2021-04-10 22:07:30 +0700

    Убрала последний цикл

--- task6_2.py
+++ task6_2.py
@@ -11,8 +11,6 @@ for act in acts:  # в цикле перебираем акты пьесы (эт
         action = scene['action']  # это список из словарей; в каждом словаре по 2 ключа:
         # "character" (то есть имя персонажа) и "says" (то есть его реплика)
         for a in action:
-            s.append((a['character'], a['says']))  # добавляем к списку s кортеж вида (персонаж, его реплика)
-for k, v in s:  # в цикле перебираем элементы списка s - кортежи
-    d[k].append(v)
+            d[a['character']].append(a['says'])
 with open('character_says.json', 'w') as f2:
     json.dump(d, f2)
