ee99fb3cdd6d7885414b42904e7ecee693ca7ba9
Daria Borodina <d.borodina@g.nsu.ru>
2021-05-17 11:28:29 +0700

    Добавила метод setUp()

--- test.py
+++ test.py
@@ -3,23 +3,19 @@ from morphology import Corpus
 
 
 class MyTestCase(unittest.TestCase):
-    def test_presence_words_in_sentence(self):
+    def setUp(self):
         corpus = Corpus()
         corpus.load('annot.opcorpora.no_ambig.xml')
-        sentence = corpus.get_sentence_by_id(id=0)
-        self.assertGreater(len(sentence._words), 0)
+        self.sentence = corpus.get_sentence_by_id(id=0)
+
+    def test_presence_words_in_sentence(self):
+        self.assertGreater(len(self.sentence._words), 0)
 
     def test_first_sentence(self):
-        corpus = Corpus()
-        corpus.load('annot.opcorpora.no_ambig.xml')
-        sentence = corpus.get_sentence_by_id(id=0)
-        self.assertEqual(sentence._sentence_str, '«Школа злословия» учит прикусить язык')
+        self.assertEqual(self.sentence._sentence_str, '«Школа злословия» учит прикусить язык')
 
     def test_grammar(self):
-        corpus = Corpus()
-        corpus.load('annot.opcorpora.no_ambig.xml')
-        sentence = corpus.get_sentence_by_id(id=0)
-        word = sentence.get_word_by_id(1)
+        word = self.sentence.get_word_by_id(1)
         self.assertIn('NOUN', word._grams)
 
 
